51ee2d455139f400fe0cb3f3440047593902e946
Darya Faitelson <daryafaitelson@gmail.com>
2025-12-15 21:18:50 +0300

    заготовка для лоадера

--- src/modules/loaders.js
+++ src/modules/loaders.js
@@ -2,8 +2,11 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 
 const loadingManager = new THREE.LoadingManager();
+loadingManager.onLoad = () => {
+  console.log("loaded");
+};
 const textureLoader = new THREE.TextureLoader(loadingManager);
-const gltfLoader = new GLTFLoader();
-const cubeTextureLoader = new THREE.CubeTextureLoader().setPath("cubeMaps/");
+const gltfLoader = new GLTFLoader(loadingManager);
+const cubeTextureLoader = new THREE.CubeTextureLoader(loadingManager).setPath("cubeMaps/");
 
 export { loadingManager, textureLoader, gltfLoader, cubeTextureLoader };
